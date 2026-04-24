15eae047ec14b15cb1394544c403eeaa60fff73c
Manuel Freitas <tbmanu03@gmail.com>
2026-04-24 00:03:19 -0500

    Fix: Agregar tipos de NextAuth correctamente

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,21 +1,23 @@
-import { DefaultSession, DefaultUser } from "next-auth";
-import { DefaultJWT } from "next-auth/jwt";
+export {};
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
       role: string;
-    } & DefaultSession["user"];
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
   }
 
-  interface User extends DefaultUser {
+  interface User {
     role: string;
   }
 }
 
 declare module "next-auth/jwt" {
-  interface JWT extends DefaultJWT {
+  interface JWT {
     id: string;
     role: string;
   }
